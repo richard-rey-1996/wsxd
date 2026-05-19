32b72ca216859fa90d61106c05b4b04fc12d37c7
richard-rey-1996 <cohenlam678@gmail.com>
2026-05-20 03:20:57 +1000

    fix

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "[g2ray] Downloading Xray latest..."
-curl -sL "https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip" -o /tmp/xray.zip
-unzip -q /tmp/xray.zip xray -d /tmp
-install -m 755 /tmp/xray /usr/local/bin/xray
-rm /tmp/xray.zip
+RELEASE="https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip"
+TMPDIR="$(mktemp -d)"
 
-echo "[g2ray] Downloading GeoIP and GeoSite..."
-curl -sL "https://github.com/v2fly/geoip/releases/latest/download/geoip.dat" -o /usr/local/bin/geoip.dat
-curl -sL "https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat" -o /usr/local/bin/geosite.dat
+curl -sL "$RELEASE" -o "$TMPDIR/xray.zip"
+unzip -q "$TMPDIR/xray.zip" -d "$TMPDIR"
+install -m 755 "$TMPDIR/xray" /usr/local/bin/xray
 
-echo "[g2ray] Done."
+rm -rf "$TMPDIR"
